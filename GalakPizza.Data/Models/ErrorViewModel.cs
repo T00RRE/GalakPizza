7f3c6e7b6ddde3ba6aa627134102d95c1f5df46b
mankf <mikolajleski5@gmail.com>
2025-11-22 13:30:44 +0100

    initial commit

---
+++ GalakPizza.Data/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GalakPizza.Data.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
